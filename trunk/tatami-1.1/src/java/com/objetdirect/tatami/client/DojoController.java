9fc895c2c5df470ec42af9c11b827c4a0b85e560
vgrassaud <vgrassaud@9c128818-f83b-0410-a236-054e43b8c817>
2008-07-18 14:52:59 +0000

    the method unlink was modify to check if the domNode of the Dojo widget is not null

--- trunk/tatami-1.1/src/java/com/objetdirect/tatami/client/DojoController.java
+++ trunk/tatami-1.1/src/java/com/objetdirect/tatami/client/DojoController.java
@@ -1,25 +1,25 @@
 /*
- * Tatami: 
+ * Tatami:
  * Copyright (C) 2007 Objet Direct
  * Copyright (C) 2007 France Telecom
  * Contact: tatami@googlegroups.com
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  * USA
  *
- * Authors: Vianney Grassaud 
+ * Authors: Vianney Grassaud
  * Initial developer(s):
  * Contributor(s):
  */
@@ -35,12 +35,12 @@ import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- * This class is a sungleton and permits to control some Dojo features. It controls the loading of 
+ * This class is a sungleton and permits to control some Dojo features. It controls the loading of
  * the scripts JavaScript those are necessaries for the Dojo widgets, ie avoids
  * to load several times the same scripts. It permits also to link or unlink the dojo widget with the GWT widget
- * and finally permits to scedule a creation of the Dojo widget to avoit some memory leak. 
- *  
- * 
+ * and finally permits to scedule a creation of the Dojo widget to avoit some memory leak.
+ *
+ *
  * @author Vianney
  */
 public class DojoController {
@@ -49,14 +49,14 @@ public class DojoController {
 	private static DojoController controller;
 
 	/**
-	 
+
 	 * The map of the declared Dojo widgets. This map  permits to know
-	 * what are the script JavaScript which are loaded in the browser. 
-	 
+	 * what are the script JavaScript which are loaded in the browser.
+
 	 */
 	private Map mapWidget;
 
-	/** 
+	/**
 	 * Creates the controller.
 	 **/
 	private DojoController() {
@@ -76,9 +76,9 @@ public class DojoController {
 
 	/**
 	 * Loads the scripts Javascripts for the DOJO widget.
-	 * @param widgetDojo the package to import in the browser for the DOJO widget. 
+	 * @param widgetDojo the package to import in the browser for the DOJO widget.
      * @return true if the loading of the script was necessary, false if the script was already loaded.
-     * @throws IllegalArgumentException if the param widgetDojo is not a package for the version of Dojo. 
+     * @throws IllegalArgumentException if the param widgetDojo is not a package for the version of Dojo.
 	 */
 	public boolean require(String widgetDojo) {
 		boolean loaded = mapWidget.containsKey(widgetDojo);
@@ -95,43 +95,46 @@ public class DojoController {
 	/**
 	 * Destroy the Dojo widget.
 	 * @param dojoWidget the DOJO widget to destroy.
-	 * 
+	 *
 	 */
 	public native void destroy(JavaScriptObject dojoWidget)
 	/*-{
 	    try {
 	     dojoWidget.destroyRecursive();
-	      
-	     } catch (e) {
-	      
+        } catch (e) {
+	      $wnd.alert("error " + e);
 	     }
 	 }-*/;
 
 	/**
-	 *  
-	 * Loads the scritps JavaScript for the Dojo widget. 
+	 *
+	 * Loads the scritps JavaScript for the Dojo widget.
 	 * @param widget the widget to import example : dojo.widget.Clock
-	 * 
+	 *
 	 */
 	private native boolean requireDojo(String widget)
 	/*-{
 	 var ok = true;
 	 try {
+
 	    $wnd.dojo.require(widget);
      } catch (e) {
-	   
 	   ok =false;
 	 }
-	 return ok;
-	 
+
+     return ok;
+
+
+
+
 	 }-*/;
 
 	/**
-	 *  
-	 * Loads if it is necessary the scripts JavaScript DOJO for a component implementing the interface 
-	 * <code>Hasdojo</code>. This method calls also <code>HasDojo#onDojoLoad()</code> if the scripts was loaded. 
+	 *
+	 * Loads if it is necessary the scripts JavaScript DOJO for a component implementing the interface
+	 * <code>Hasdojo</code>. This method calls also <code>HasDojo#onDojoLoad()</code> if the scripts was loaded.
      * @param widget the widget implementing HasDojo interface.
-	 * 
+	 *
 	 */
 	public void loadDojoWidget(HasDojo widget) {
 		try {
@@ -146,7 +149,7 @@ public class DojoController {
 
 	/**
 	 * Creates a dojo widget from an object implementing the  interface <code>HasDojo</code>.
-	 * Links also the dojo Widget with GTW widget. 
+	 * Links also the dojo Widget with GTW widget.
 	 * @param widget the widget implementing the HasDojo interface.
 	 * @param gwtWidget the GWT widget to link with the Dojo widget.
 	 * @see #link(JavaScriptObject, Widget)
@@ -166,7 +169,7 @@ public class DojoController {
 
 	/**
 	 * Removes a link between a GWT widget and a DOJO widget. The DOJO widget
-	 * will be also removed from the GWT widget. 
+	 * will be also removed from the GWT widget.
 	 * @param widget the widget containing the DOJO widget.
 	 * @param gwtWidget the gwt widget linked with the DOJO widget.
 	 */
@@ -180,11 +183,11 @@ public class DojoController {
 	}
 
 	/**
- 
+
 	 * Creates the link between a DOJO widget et the GWT widget. Adds to DOM element of the GWT widget, the DOM element
 	 * of the DOJO widget.
 	 * @param dojoWidget the DOJO widget to link
-	 * @param gwtWidget the GWT widget to link 
+	 * @param gwtWidget the GWT widget to link
 	 */
 	public void link(JavaScriptObject dojoWidget, Widget gwtWidget) {
 		if (dojoWidget != null) {
@@ -195,27 +198,34 @@ public class DojoController {
 	}
 
 	/**
- 
+
 	 * Removes a link between a GWT widget and a DOJO widget. The DOJO widget
-	 * will be also removed from the GWT widget. 
+	 * will be also removed from the GWT widget.
 	 * @param dojoWidget the DOJO widget.
 	 * @param gwtWidget the gwt widget linked with the DOJO widget.
 	 */
 	public void unLink(JavaScriptObject dojoWidget, Widget gwtWidget) {
 		setGWTWidget(dojoWidget, null);
-		DOM.removeChild(gwtWidget.getElement(), getDomNodeDojo(dojoWidget));
+        Element domNode = getDomNodeDojo(dojoWidget);
+        if ( domNode != null) {
+		 DOM.removeChild(gwtWidget.getElement(), domNode);
+        }
 	}
 
 	/**
- 
+
 	 * Returns the DOM element that correponding to the DOJO widget.
 	 * @param dojoWidget the DOJO widget.
 	 * @return the DOM element of the given DOJO widget
-	 * 
+	 *
 	 */
 	private native Element getDomNodeDojo(JavaScriptObject dojoWidget)
 	/*-{
-   	    return dojoWidget.domNode;
+	    var element = null;
+   	    if ( dojoWidget.domNode != "undefined" && dojoWidget.domNode != null ){
+   	       element= dojoWidget.domNode;
+   	    }
+   	    return element;
 	 }-*/;
 
 
@@ -226,7 +236,7 @@ public class DojoController {
 	static private native void startup(JavaScriptObject dojoWidget) /*-{
 		dojoWidget.startup();
 	}-*/;
-	
+
 	/**
 	 * Calls the startup method on a widget which contains a DOJO widget
 	 * @param widget a widget wrapping a DOJO widget
@@ -234,26 +244,26 @@ public class DojoController {
 	static public void startup(HasDojo widget) {
 		startup(widget.getDojoWidget());
 	}
-	
-	
+
+
 	/**
- 
+
 	 * Returns the DOM element that correponding to the DOJO widget.
 	 * @param widget a widget containing a DOJO widget.
 	 * @return the DOM element fulfiling the given widget
-	 * 
+	 *
 	 */
 	public Element getDomNode(HasDojo widget) {
 		return getDomNodeDojo(widget.getDojoWidget());
 	}
-	
-	
+
+
 	/**
- 
+
 	 * Creates or deletes the link between the DOJO widget and the GWT widget.
      * @param dojoWidget the DOJO widget.
-     * @para  gwtWidget the GWT widget. 
-	 * 
+     * @para  gwtWidget the GWT widget.
+	 *
 	 * @see #link(JavaScriptObject, Widget)
 	 */
 	public native void setGWTWidget(JavaScriptObject dojoWidget,Widget gwtWidget)
@@ -265,9 +275,9 @@ public class DojoController {
 	 }-*/;
 
 	/**
- 
+
 	 * Creates a JavaScript alert
-	 * @param object the message to display in the alert.  
+	 * @param object the message to display in the alert.
 	 * NB: if you want to debug, use the <code>GWT.log(String, Throwable)</code> method instead
 
 	 */
@@ -288,7 +298,7 @@ public class DojoController {
     	 }
     	 return jsArray;
      }
-	
+
      /**
       * Puts a string at the specified index in a JavaScript array
       * @param array the JavaScript array
@@ -300,7 +310,7 @@ public class DojoController {
          array[index] = value;
          return array;
      }-*/;
-	
+
      /**
       * Removes a <code>JavaScriptObject</code> from the DOJO widget
       * @param widget the GWT widget containing the DOJO widget
@@ -309,30 +319,30 @@ public class DojoController {
      public void removeChild(HasDojo widget,JavaScriptObject child) {
     	 removeChild(widget.getDojoWidget(),child);
      }
-     
+
      /**
       * Adds a <code>JavaScriptObject</code> as a child for the DOJO widget
-      * @param widget the GWT widget containing the DOJO widget 
+      * @param widget the GWT widget containing the DOJO widget
       * @param child the <code>JavaScriptObject</code> to add
       */
      public void addChild(HasDojo widget,JavaScriptObject child) {
     	 addChild(widget.getDojoWidget(),child);
      }
-     
+
      /*
       * @see #removeChild(HasDojo,JavaScriptObject)
-      */ 
+      */
      private native void removeChild(JavaScriptObject dojoWidget, JavaScriptObject child)/*-{
         dojoWidget.removeChild(child);
      }-*/;
-	
+
      /*
       * @see #addChild(HasDojo,JavaScriptObject)
-      */ 
+      */
       private native void addChild(JavaScriptObject dojoWidget, JavaScriptObject child) /*-{
         dojoWidget.addChild(child);
 	    dojoWidget.startup();
       }-*/;
-	
+
 
 }//end of class
