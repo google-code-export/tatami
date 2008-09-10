def826c63fbe9ed662b2c02ca1a8f473cf68c7ed
rdunklau <rdunklau@9c128818-f83b-0410-a236-054e43b8c817>
2008-09-10 14:01:47 +0000

    Undone commit to the wrong place

--- src/java/com/google/gwt/user/client/ui/Helper.java
+++
@@ -1,9 +0,0 @@
-package com.google.gwt.user.client.ui;
-
-import com.google.gwt.dom.client.Element;
-
-public class Helper {
-	public static void replaceElement(UIObject uiObject , Element elem){
-		uiObject.replaceElement(elem); 
-	}
-}
