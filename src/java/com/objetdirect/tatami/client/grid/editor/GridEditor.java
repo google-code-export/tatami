75abe91bb1776ffc0d01e6d1a80da02e2dbed88d
rdunklau <rdunklau@9c128818-f83b-0410-a236-054e43b8c817>
2008-09-11 09:29:38 +0000

    made a copy

---
+++ src/java/com/objetdirect/tatami/client/grid/editor/GridEditor.java
@@ -0,0 +1,52 @@
+/*
+ * Tatami: 
+ * Copyright (C) 2007 Objet Direct
+ * Copyright (C) 2007 France Telecom
+ * Contact: tatami@googlegroups.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * Authors:  Ronan Dunklau
+ * Initial developer(s): Ronan Dunklau
+ * Contributor(s):
+ */
+package com.objetdirect.tatami.client.grid.editor;
+
+import java.util.Map;
+
+import com.objetdirect.tatami.client.HasDojo;
+
+/**
+ * This interface defines a grid editor
+ * 
+ * @author rdunklau
+ *
+ */
+public interface  GridEditor   {
+	
+	/**
+	 * @return editor's dojo class name, used by dojo to instantiate the editor 
+	 * when the cell is edited
+	 */
+	public  String getDojoGridEditorName();
+	
+	/**
+	 * @return editor's options
+	 */
+	public  Map getAttributes();
+	
+	
+}
