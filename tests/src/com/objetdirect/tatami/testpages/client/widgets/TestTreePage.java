5eae997832a4564c36add30cc1b6dcb0eab9faa9
rdunklau <rdunklau@9c128818-f83b-0410-a236-054e43b8c817>
2008-11-20 16:06:59 +0000

    Adding tatami-tests project

---
+++ tests/src/com/objetdirect/tatami/testpages/client/widgets/TestTreePage.java
@@ -0,0 +1,100 @@
+/*
+ * Tatami: 
+ * Copyright (C) 2007 Objet Direct
+ * Copyright (C) 2007 France Telecom
+ * Contact: tatami@googlegroups.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * Authors:  Ronan Dunklau
+ * Initial developer(s): Ronan Dunklau
+ * Contributor(s):
+ */
+package com.objetdirect.tatami.testpages.client.widgets;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.objetdirect.tatami.client.data.Item;
+import com.objetdirect.tatami.client.tree.Tree;
+import com.objetdirect.tatami.client.tree.TreeListener;
+import com.objetdirect.tatami.testpages.client.TestPage;
+
+public class TestTreePage extends TestPage{
+
+	public TestTreePage(){
+		 super(TestTreePage.class.getName(), "Test Tree");
+	}
+	
+	public Widget getTestPage() {
+		final Tree tree = new Tree(new Item("Root","Root"));
+		DOM.setElementAttribute(tree.getElement(), "id", "TreeContainer");
+		final Item item1 = new Item("item1" , "Item 1");
+		final Item item11 = new Item("item1.1" , "Item 1.1");
+		final Item item12 = new Item("item1.2" , "Item 1.2");
+		item1.addChild(item11);
+		item1.addChild(item12);
+		item12.addChild(new Item("item1.2.1" , "Item 1.2.1"));
+		item12.addChild(new Item("item1.2.2" , "Item 1.2.2"));
+		item1.setValue(Tree.labelClassAttribute,"myLabelClass");
+		
+		final Item item2 = new Item("item2" , "Item 2");
+		tree.setDefaultFolderClosedClass("myTreeClosed");
+		tree.setDefaultFolderOpenClass("myTreeOpened");
+		tree.getRootItem().addChild(item1);
+		tree.getRootItem().addChild(item2);
+		//tree.setShowRoot(false);
+		HorizontalPanel panel = new HorizontalPanel();
+		final HTML openValue = new HTML("Opened :");
+		final HTML closedValue = new HTML("Closed :");
+		final HTML onClickValue = new HTML("Clicked :");
+		final HTML onDblClickValue = new HTML("DblClicked :");
+		panel.add(tree);
+		tree.addTreeListener(new TreeListener() {
+			public void onOpen(Item item) {
+				openValue.setHTML("Opened :" + tree.getStore().getIdentity(item));
+			}
+			public void onClose(Item item) {
+				closedValue.setHTML("Closed :" + tree.getStore().getIdentity(item));
+			}
+			public void onClick(Item item) {
+				onClickValue.setHTML("Clicked :" + tree.getStore().getIdentity(item));
+			}
+			public void onDblClick(Item item) {
+				onDblClickValue.setHTML("DblClicked :" + tree.getStore().getIdentity(item));
+			}
+		});
+		
+		DOM.setElementAttribute(openValue.getElement(), "id", "TreeOpenValue");
+		DOM.setElementAttribute(closedValue.getElement(), "id", "TreeClosedValue");
+		DOM.setElementAttribute(onClickValue.getElement(), "id", "TreeClickValue");
+		DOM.setElementAttribute(onDblClickValue.getElement(), "id", "TreeDblClickValue");
+		Item newItem =  new Item("item3" , "Item 3");
+		tree.getRootItem().addChild(newItem);
+		Item newItem2 =  new Item("item3.1" , "Item 3.1");
+		newItem.addChild(newItem2);
+		tree.setSize("200px", "200px");
+		panel.add(onClickValue);
+		panel.add(onDblClickValue);
+		panel.add(closedValue);
+		panel.add(openValue);
+		return panel;
+	}
+
+}
---
+++ tests/test/com/objetdirect/tatami/test/widgets/TestSimplestGrid.java
@@ -0,0 +1,127 @@
+package com.objetdirect.tatami.test.widgets;
+
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.objetdirect.tatami.testpages.client.widgets.TestSimpleGridPage;
+import com.objetdirect.tatami.unit.TatamiTestCase;
+import com.objetdirect.tatami.unit.TestGWT;
+import com.objetdirect.tatami.unit.wrappers.GridWrapper;
+
+public class TestSimplestGrid extends AbstractTestWidgets{
+
+	GridWrapper grid;
+	
+	HtmlElement lastClickedCell;
+	HtmlElement lastClickedColumnField;
+	HtmlElement lastChangedValue;
+	HtmlElement lastRowSelection;
+	
+	
+	HtmlElement sortBySecondColumnButton;
+	HtmlElement addRowButton;
+	HtmlElement removeRow1Button;
+	HtmlElement removeSelectedRows;
+	HtmlElement addColumnButton;
+	HtmlElement removeColumnButton;
+	HtmlElement clearGridButton;
+	
+
+	protected String getTestPageId() {
+		return TestSimpleGridPage.class.getName();
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		testGwt.waitForBackgroundTasksToComplete(5000);
+		grid =  new GridWrapper(page , "GridContainer");
+		
+		lastClickedCell = (HtmlElement) page.getElementById("LastClickedCell");
+		lastClickedColumnField = (HtmlElement) page.getElementById("LastClickedColumnField");
+		lastChangedValue = (HtmlElement) page.getElementById("LastDataChange");
+		lastRowSelection = (HtmlElement) page.getElementById("SelectedRows");
+		
+		sortBySecondColumnButton = (HtmlElement) page.getElementById("SetSecondColumnOrderButton");
+		addRowButton = (HtmlElement) page.getElementById("AddRowButton");
+		removeRow1Button = (HtmlElement) page.getElementById("RemoveRowNumber1Button");
+		removeSelectedRows = (HtmlElement) page.getElementById("RemoveSelectedRowsButton");
+		addColumnButton = (HtmlElement) page.getElementById("AddColumnButton");
+		removeColumnButton = (HtmlElement) page.getElementById("RemoveColumnButton");
+		clearGridButton = (HtmlElement) page.getElementById("ClearGridButton");
+	}
+	
+	
+	public void testGridInit(){
+		String firstCellContent = grid.getCell(0,0).getTextContent();
+		String firstHeaderCellContent = grid.getCellHeader(0).getTextContent();
+		assertEquals("B", firstCellContent);
+		assertEquals("First Column", firstHeaderCellContent);
+		grid.clickOnCell(1,1);
+		testGwt.waitForBackgroundTasksToComplete(400);
+		assertEquals("1:1",lastClickedCell.getTextContent());
+		grid.clickOnCellHeader(0);
+		testGwt.waitForBackgroundTasksToComplete(100);
+		firstCellContent = grid.getCell(0,0).getTextContent();
+		assertEquals("A", firstCellContent);
+		grid.clickOnCellHeader(0);
+		firstCellContent = grid.getCell(0,0).getTextContent();
+		assertEquals("C", firstCellContent);
+	}
+	
+	public void testSort(){
+		grid.clickOnCellHeader(0);
+		String secondCellContent = grid.getCell(1,0).getTextContent();
+		String firstCellContent = grid.getCell(0,0).getTextContent();
+		assertEquals("A", firstCellContent);
+		assertEquals("B", secondCellContent);
+		grid.clickOnCellHeader(0);
+		firstCellContent = grid.getCell(0,0).getTextContent();
+		assertEquals("C", firstCellContent);
+		testGwt.mouseClick(sortBySecondColumnButton, TestGWT.BUTTON_LEFT, false, false, false);
+		firstCellContent = grid.getCell(0,1).getTextContent();
+		assertEquals("a", firstCellContent);
+		
+	}
+	
+
+	
+	public void testAddAndRemoveRow(){
+		testGwt.mouseClick(addRowButton, TestGWT.BUTTON_LEFT, false, false, false);
+		assertEquals(4, grid.getRowCount());
+		assertEquals("D", grid.getCell(3, 0).getTextContent());
+		testGwt.mouseClick(removeRow1Button, TestGWT.BUTTON_LEFT, false, false, false);
+		assertEquals(3, grid.getRowCount());
+		HtmlElement cell1 = grid.getCell(2, 0);
+		assertEquals("D", cell1.getTextContent());
+		assertEquals("B", grid.getCell(0, 0).getTextContent());
+	}
+	
+	public void testSelectRows(){
+		grid.clickOnCell(0,0);
+		assertEquals("0", lastRowSelection.getTextContent());
+		grid.clickOnCell(0,0);
+		testGwt.mouseClick(grid.getCell(2,0), TestGWT.BUTTON_LEFT, false, true, false);
+		assertEquals("0-1-2", lastRowSelection.getTextContent());
+		grid.clickOnCell(1,0);
+		assertEquals("1", lastRowSelection.getTextContent());
+		testGwt.mouseClick(grid.getCell(0,0), TestGWT.BUTTON_LEFT, false, false, true);
+		assertEquals("0-1", lastRowSelection.getTextContent());
+		testGwt.mouseClick(removeSelectedRows, TestGWT.BUTTON_LEFT, false, false, false);
+		assertEquals(1 , grid.getRowCount());
+		assertEquals("C", grid.getCell(0,0).getTextContent());
+	}
+	
+	public void testAddRemoveColumns(){
+		testGwt.mouseClick(addColumnButton, TestGWT.BUTTON_LEFT, false, false, false);
+		assertEquals("c" , grid.getCell(0,2).getTextContent());
+		testGwt.mouseClick(removeColumnButton, TestGWT.BUTTON_LEFT, false, false, false);
+		assertEquals("1" , grid.getCell(0,2).getTextContent());
+	}
+	
+	public void testClearGrid(){
+		assertEquals(3,grid.getRowCount());
+		testGwt.mouseClick(clearGridButton, TestGWT.BUTTON_LEFT, false, false, false);
+		assertEquals(0,grid.getRowCount());
+	}
+	
+	
+}
