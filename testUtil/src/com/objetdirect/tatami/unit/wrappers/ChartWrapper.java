8b33ded8e7c3931b722d4155522e98e0cd667582
rdunklau <rdunklau@9c128818-f83b-0410-a236-054e43b8c817>
2008-11-24 09:07:56 +0000

    Added a chart wrapper. Currently allowing to retrive PiePiece and CircleMarkers shape from a chart

--- testUtil/src/com/objetdirect/tatami/unit/wrappers/ChartWrapper.java
+++ testUtil/src/com/objetdirect/tatami/unit/wrappers/ChartWrapper.java
@@ -1,5 +1,8 @@
 package com.objetdirect.tatami.unit.wrappers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.objetdirect.tatami.unit.TestGWT;
@@ -17,6 +20,28 @@ public class ChartWrapper {
 	public ChartWrapper(HtmlElement elem) throws Exception{
 		this.testGwt = new TestGWT(elem.getPage().getEnclosingWindow());
 		this.chartBaseElement = elem;
+	}
+	
+	public List<HtmlElement> getPiePieces(){
+		List<HtmlElement> elems = chartBaseElement.getHtmlElementsByTagName("path");
+		List<HtmlElement> pies = new ArrayList<HtmlElement>();
+		for (HtmlElement htmlElement : elems) {
+			if(htmlElement.getAttribute("d").startsWith("M 150 150L") && htmlElement.getAttribute("d").endsWith("150 150Z")){
+				pies.add(htmlElement);
+			}
+		}
+		return pies;
+	}
+	
+	public List<HtmlElement> getCircleMarkers(){
+		List<HtmlElement> elems = chartBaseElement.getHtmlElementsByTagName("path");
+		List<HtmlElement> markers = new ArrayList<HtmlElement>();
+		for (HtmlElement htmlElement : elems) {
+			if(htmlElement.getAttribute("d").endsWith("m-3,0 c0,-4 6,-4 6,0 m-6,0 c0,4 6,4 6,0")){
+				markers.add(htmlElement);
+			}
+		}
+		return markers;
 	}
 	
 }
