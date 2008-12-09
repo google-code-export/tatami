160136f086d3900d5a1eb350eb0c236fad8f978a
rdunklau <rdunklau@9c128818-f83b-0410-a236-054e43b8c817>
2008-12-09 17:42:09 +0000

    Added the BorderContainer layout, the ContentPanel and stuff

---
+++ core/src/java/com/objetdirect/tatami/client/layout/DojoPanel.java
@@ -0,0 +1,21 @@
+package com.objetdirect.tatami.client.layout;
+
+import com.google.gwt.user.client.ui.Widget;
+import com.objetdirect.tatami.client.HasDojo;
+
+/**
+ * This interface is designed as a "marker" for Tatami's panel implementations
+ * compatible with dojo layouts. 
+ * 
+ * It as only one method, which implementation should usally return the panel itself,
+ * since it is intended for it to be an actual GWT Panel subclass.
+ * 
+ * @author rdunklau
+ *
+ */
+public interface DojoPanel extends HasDojo{
+	/**
+	 * @return the dojopanel as a widget, to avoid unecessary cast operations
+	 */
+	public Widget asWidget();
+}
